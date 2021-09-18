e2c8235a4e2a6be16248b16b3b1b8a8e2b7f3987
Denta Bramasta Hidayat <denta.dent53@gmail.com>
2021-09-18 20:24:42 +0700

    css and media query

--- assets/cv.js
+++ assets/cv.js
@@ -1,35 +1,10 @@
 let header = document.querySelector("header");
-const text = document.querySelector(".font");
-const strText = text.textContent;
-const splitText = strText.split("");
-text.textContent = "";
-const menuToggle = document.querySelector(".menu-icon");
+const menuOpen = document.querySelector(".menu-icon");
 const nav = document.querySelector("header ul");
-
-for (let i = 0; i < splitText.length; i++) {
-  text.innerHTML += "<span>" + splitText[i] + "</span>";
-}
-
-let char = 0;
-let timer = setInterval(onTick, 50);
-function onTick() {
-  const span = text.querySelectorAll("span")[char];
-  span.classList.add("fade");
-  char++;
-  if (char === splitText.length) {
-    complete();
-    return;
-  }
-}
-
-function complete() {
-  clearInterval(timer);
-  timer = null;
-}
 
 window.addEventListener("scroll", function () {
   let value = window.scrollY;
-  header.classList.toggle("sticky", window.scrollY > 150);
+  header.classList.toggle("sticky", window.scrollY > 100);
 });
 
 window.addEventListener("click", function () {
